da70487deb41a4a5de906b13b0d26e16b63fd37e
winky winky face <85230219+Nawiss8@users.noreply.github.com>
2025-09-15 16:48:40 +0200

    heeey premier

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+
+
+Route::get('/home', function () {
+    return view('welcome');
+});
